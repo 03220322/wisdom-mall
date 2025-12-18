ea36ac870402ea4844c522e3aeaedb03183f4136
xiaoztongx@qq.com <xiaoztongx@qq.com>
2025-12-18 11:02:32 +0800

    add github actions for deploy

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
-  publicPath: './', // 原来是'/'
+  // 生产环境用仓库名作为公共路径，开发环境用 /
+  publicPath: process.env.NODE_ENV === 'production' ? '/wisdom-mall/' : '/',
   transpileDependencies: true,
 });
